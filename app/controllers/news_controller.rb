ab9ca0e39709ccda64996418491bfc0032eff466
sgemini <13268001833@163.com>
2016-04-07 21:55:58 +0800

    yayaya

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,15 @@
 class NewsController < ApplicationController
+	def index
+		news = News.all
+	end
+
+	def show
+		news = News.find_by(news_params)
+	end
+
+	private
+
+	def news_params
+		params.require(:news).permit(:id, :title, :content)
+	end
 end
