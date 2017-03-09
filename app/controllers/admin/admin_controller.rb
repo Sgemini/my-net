6de3a7900b1aca2c9aee835e090b83aba70d1168
sgemini <sgemini@sgeminideMacBook-Pro.local>
2017-03-10 00:10:33 +0800

    add authentication in admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,5 @@
 class Admin::AdminController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def index
     @all_tables = ActiveRecord::Base.connection.tables
   end
