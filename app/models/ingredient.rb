ac3f57e9d162ed22984de287d9922c12d4470fa6
sgemini <s13268001833@gmail.com>
2017-04-03 14:02:21 +0800

    add admin

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ActiveRecord::Base
-  has_many :cates, through: :cates_ingredients_map
+  has_many :cates_ingredients_maps
+  has_many :cates, through: :cates_ingredients_maps
 end
