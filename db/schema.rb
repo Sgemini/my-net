83ac7ae80ca1f65f6e1316b4574944cf807cc4c3
vliang <vliang@xogrp.com>
2017-04-24 17:20:23 +0800

    add yang web

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 20170403043029) do
   create_table "cates", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cates_ingredients_maps", force: :cascade do |t|
@@ -54,10 +54,8 @@ ActiveRecord::Schema.define(version: 20170403043029) do
   end
 
   create_table "live_tips", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
+    t.text   "body"
   end
 
 end
